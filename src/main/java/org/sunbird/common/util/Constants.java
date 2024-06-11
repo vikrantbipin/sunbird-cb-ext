fa637446eaba91ca1ee2470c43b24e72e13569ff
ravisaurav-tarento <142487505+ravisaurav-tarento@users.noreply.github.com>
2024-06-11 11:20:29 +0530

    4.8.14 dev v9 (#590)
    * 4.8.15 dev v1 (#584)
    
    * KB-4705 | DEV| Assessment | BE | Consumption Logic for the QuestionWeightage Assessment Type (#564)
    
    * Added string handling in admin patch API
    
    * Assessment V5 Changes
    
    Assessment V5 Changes
    
    * KB-4705 | DEV| Assessment | BE | Consumption Logic for the QuestionWeightage Assessment Type
    
    1. Removed the hardcoded attributes and added from assessment hierarchy.
    
    * KB-4705 | DEV| Assessment | BE | Consumption Logic for the QuestionWeightage Assessment Type
    
    1. Added proper comments and logs.
    2. Refractored the code.
    3. Added logic for optionweightage.
    
    ---------
    
    Co-authored-by: karthik-tarento <karthikeyan.rajendran@tarento.com>
    
    * Retry Attempts Enabled for Assessment V5
    
    * KB-4705 | DEV| Assessment | BE | Consumption Logic for the QuestionWeightage Assessment Type (#565)
    
    1. Optional weightage score calculation enhancements.
    
    * Adding the insight API implementation for MDO channel (#571)
    
    * SaveStateMethod
    
    * Maintaining the order of the fields.
    
    * KB-4705 | DEV| Assessment | BE | Consumption Logic for the QuestionWeightage Assessment Type (#580)
    
    1. Enhanchements for questionScheme.
    
    * KB-5130 | DEV| Assessment | BE | Enhancement in Consumption Logic for the QuestionWeightage Assessment Type. (#581)
    
    1. Added logic for new attributes added.
    2. Added questionLevel param in Question read API.
    
    * KB-5130 | DEV| Assessment | BE | Enhancement in Consumption Logic for the QuestionWeightage Assessment Type. (#583)
    
    1. Score calculation fix.
    
    ---------
    
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
    Co-authored-by: karthik-tarento <karthikeyan.rajendran@tarento.com>
    Co-authored-by: saipradeep_ravipati <saipradeep.ravipati@tarento.com>
    
    * KB-4754:Validation for language, designation fix
    
    * removed changes from cbrelaease-4.8.15
    
    * Modified error message on date of birth validation failure
    
    * changed approach for fetchind and storing data from redis
    
    ---------
    
    Co-authored-by: Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
    Co-authored-by: tarentomaheshvakkund <139739142+tarentomaheshvakkund@users.noreply.github.com>
    Co-authored-by: karthik-tarento <karthikeyan.rajendran@tarento.com>
    Co-authored-by: saipradeep_ravipati <saipradeep.ravipati@tarento.com>

--- src/main/java/org/sunbird/common/util/Constants.java
+++ src/main/java/org/sunbird/common/util/Constants.java
@@ -1063,6 +1063,7 @@ public class Constants {
 	public static final String PROFILE_GROUP_STATUS = "profileGroupStatus";
 	public static final String PROFILE_DESIGNATION_STATUS = "profileDesignationStatus";
 	public static final String NOT_MY_USER = "NOT-MY-USER";
+	public static final String SPACE = " ";
 
 	private Constants() {
 		throw new IllegalStateException("Utility class");

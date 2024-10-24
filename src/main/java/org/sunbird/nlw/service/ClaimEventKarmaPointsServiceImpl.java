b587a9156c57f23db9aae02de8fca79c97e9842f
vikrantsingh <vikrant.bipin@tarento.com>
2024-10-24 14:55:35 +0530

    "Claim karma point event and corrected ets time"

--- src/main/java/org/sunbird/nlw/service/ClaimEventKarmaPointsServiceImpl.java
+++ src/main/java/org/sunbird/nlw/service/ClaimEventKarmaPointsServiceImpl.java
@@ -3,8 +3,10 @@ package org.sunbird.nlw.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.sunbird.common.util.CbExtServerProperties;
+import org.sunbird.common.util.Constants;
 import org.sunbird.core.producer.Producer;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,9 +20,9 @@ public class ClaimEventKarmaPointsServiceImpl {
     @Autowired
     CbExtServerProperties serverProperties;
 
-    public void generateKarmaPointEventAndPushToKafka(String userId, String eventId, String batchId) {
+    public void generateKarmaPointEventAndPushToKafka(String userId, String eventId, String batchId, Map<String, Object> eventDetails) {
 
-        long ets = System.currentTimeMillis();
+        long ets = ((Date) eventDetails.get(Constants.END_DATE)).getTime() - 10 * 1000;
 
         Map<String, Object> objectMap = new HashMap<>();
         objectMap.put("user_id", userId);
